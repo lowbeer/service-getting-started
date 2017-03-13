928233063fdd3f8101d8ea34e2649eedc6298293
Guilherme Rodrigues <gadr90@gmail.com>
2017-03-13 15:13:11 -0300

    Add mem usage response

--- service/main.js
+++ service/main.js
@@ -1,6 +1,10 @@
 module.exports = {
   handler: async (req, res, ctx) => {
     res.status = 200
-    res.body = 'Hello'
+    const arr = new Array()
+    for (let i = 0; i < 10000; i++) {
+      arr.push(new Array(Math.ceil(Math.random() * 10)))
+    }
+    res.body = JSON.stringify({mem: process.memoryUsage(), arr: arr.slice(0, 3)})
   }
 }
