35406d74bb946b32c5fc5ffc55dc8964d70bbf5a
Guilherme Rodrigues <gadr90@gmail.com>
2017-06-06 16:44:01 -0300

    Use new runtime args

--- service/main.js
+++ service/main.js
@@ -1,10 +1,6 @@
 module.exports = {
   handler: async (req, res, ctx) => {
     res.status = 200
-    const arr = new Array()
-    for (let i = 0; i < 10000; i++) {
-      arr.push(new Array(Math.ceil(Math.random() * 10)))
-    }
-    res.body = JSON.stringify({mem: process.memoryUsage(), arr: arr.slice(0, 3)})
+    res.body = JSON.stringify({mem: process.memoryUsage(), hello: 'Hi, there!'})
   }
 }
