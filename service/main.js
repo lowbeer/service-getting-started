67feece6b63534041797590fc3ae55c36a085cc5
amoreira <anderson.moreira@vtex.com.br>
2017-08-31 17:27:40 -0300

    Teste new interface

--- service/main.js
+++ service/main.js
@@ -1,9 +1,30 @@
+// main.js:
+
 module.exports = {
-  handler: async (req, res, ctx) => {
-    res.status = 200
-    res.body = JSON.stringify({
-      mem: process.memoryUsage(),
-      hello: 'Hi, there!'
-    })
-  }
+  routes: {
+    async getBanana (ctx): Promise<void> {
+      ctx.status = 200
+      ctx.body = 'Toma uma banana aí!'
+    },
+    async createBanana (ctx): Promise<void> {
+      const {name, type} = ctx.req.body
+      ctx.status = 200
+      ctx.body = `A Banana ${name} do tipo ${type} foi criada.`
+    },
+    apple (ctx): void {
+      const {method} = ctx
+      ctx.status = 200
+      ctx.body = `Apple: Eu recebi o method ${method}.`
+    },
+  },
+  events: {
+    onAppsEvent (ev) {
+      const {body} = ev
+      console.log(`onAppsEvent body: ${body}`)
+    },
+    onBundleReady (ev) {
+      const {body} = ev
+      console.log(`onBundleReady body: ${body}`)
+    },
+  },
 }
