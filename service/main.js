e7003e9971951bf882b2ebc42fb410d562fa22e2
Victor Elias <victorgelias@gmail.com>
2017-09-05 18:47:00 -0300

    Add public routes to main.js

--- service/main.js
+++ service/main.js
@@ -1,9 +1,9 @@
 module.exports = {
   handler: async (req, res, ctx) => {
     res.status = 200
-    res.body = JSON.stringify({
-      mem: process.memoryUsage(),
-      hello: 'Hi, there!'
-    })
+    res.body = "Hello, IO!"
+  },
+  routes: {
+    "/api/vtex/hello": {}
   }
 }
